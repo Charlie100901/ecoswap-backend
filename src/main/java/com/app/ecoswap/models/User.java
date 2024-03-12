bcb29a917bb5f764b21e7f020f33a7276a1efda7
carlos <carlosdelarosa1009@gmail.com>
2024-03-11 22:00:48 -0500

    endpoints basicos del Usuario

---
+++ src/main/java/com/app/ecoswap/models/User.java
@@ -0,0 +1,88 @@
+package com.app.ecoswap.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String address;
+    @Column
+    private int cellphone_number;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCellphone_number() {
+        return cellphone_number;
+    }
+
+    public void setCellphone_number(int cellphone_number) {
+        this.cellphone_number = cellphone_number;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", address='" + address + '\'' +
+                ", cellphone_number=" + cellphone_number +
+                '}';
+    }
+}
