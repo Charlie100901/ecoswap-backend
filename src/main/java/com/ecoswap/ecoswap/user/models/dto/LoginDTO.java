87fd7dbf5f1492f9d1f96eb0de710816c537afe1
-A L D A I R- <112210730+2A2G@users.noreply.github.com>
2025-05-07 20:32:59 +0000

    prueba_adrian

--- src/main/java/com/ecoswap/ecoswap/user/models/dto/LoginDTO.java
+++ src/main/java/com/ecoswap/ecoswap/user/models/dto/LoginDTO.java
@@ -1,17 +1,1 @@
-package com.ecoswap.ecoswap.user.models.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-public class LoginDTO {
-    @NotBlank
-    private String username;
-    @NotBlank
-    private String password;
-
-}
+// 
